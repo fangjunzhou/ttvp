2dc7e6e3c8c3a110ba6e99f7a50a63f5ff1ef437
Slijeff <jeff83636tx@gmail.com>
2023-02-25 17:35:13 +0000

    Update: image_resize.py

--- scripts/image_resize.py
+++ scripts/image_resize.py
@@ -1,7 +1,18 @@
 from os import listdir
 from PIL import Image
+import cv2
 
-IMAGE_DIR = ""
-OUTPUT_DIR = ""
+IMAGE_DIR = "/home/jeff/TTVP/data/thumnailUS"
+OUTPUT_DIR = "/home/jeff/TTVP/data/resizedThumbnails"
 
 images = listdir(IMAGE_DIR)
+for image in images:
+    i = Image.open(IMAGE_DIR + "/" + image).convert("RGB")
+    i = i.crop((0, 45, 480, 315))
+    i = i.resize((224, 224), Image.BILINEAR)
+    i.save(OUTPUT_DIR + "/" + image)
+
+# i = Image.open(IMAGE_DIR + "/" + images[0]).convert("RGB")
+# i = i.crop((0, 45, 480, 315))
+# i = i.resize((224, 224), Image.BILINEAR)
+# i.save(OUTPUT_DIR + "/" + images[0])
