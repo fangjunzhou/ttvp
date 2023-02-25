c0d18a602dbc7630e69ce095cb08fdc5dfb66f44
Nan Noda <nananananoda@gmail.com>
2023-02-25 23:33:37 +0000

    Update: working cv API

--- cv_api.py
+++ cv_api.py
@@ -1,3 +1,22 @@
+import torch
+from cv import ContentToViews
+import torchvision.transforms as transforms
+
+cv_model = ContentToViews()
+cv_model.load_state_dict(torch.load("best_cv_model.pt"))
+
 
 def predict_image(image)->float:
-    return 1
+    """
+    Predict the number of views for an image
+    :param image: a PIL image
+    :return: the number of views
+    """
+    transform = transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.ToTensor(),
+        transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
+    ])
+    image = transform(image).unsqueeze(0)
+    return cv_model(image).item()
+
